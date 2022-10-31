af688119d26307a5392ae3d450b1f1cc19949ab6
jrz_2022mb <>
2022-10-31 15:45:46 -0400

    recording for 31-oct

--- homefinder/serv.py
+++ homefinder/serv.py
@@ -3,22 +3,90 @@
 # This code should work in both Python 2 and 3
 from flask import *
 import json, csv
+from pprint import pprint
 
-app = Flask(__name__, static_url_path='')
+# Main app used to interface with the flask library
+# Decorators will make use of this, but in many cases we will call
+#  functions straight from flask such as send_from_directory and jsonify
+app = Flask(__name__, static_url_path='')  # By default, looks in ./static
 
+# Root path serves up index.html from the static directory -- we'll be editing this for the d3 part of the lecture
+@app.route('/')
+def index():
+    return app.send_static_file('index.1107.prof.html')
 
+@app.route('/css/<path:path>')
+def send_css(path):
+    return send_from_directory('css', path)    
+    
+    
+# Import topoJSON data to serve to client
+map = {}
+with open('pittsburgh_neighborhoods.json') as f:
+    map = json.loads( f.read() )
+    
+@app.route('/map')
+def pghmap():
+    return jsonify( map )    
 
+# jsonify is a built-in flask command that creates a JSON response out of most Py objects    
+    
+# Make a flat dictionary "database" for our use -- this should be not be used in a production system
+# Pick a more resilient option (or even just SQLite) if you're going to try this in practice
 
-
-
-
+db = {}
 # HouseID => {'HouseID','Neighborhood','Latitude','Longitude','Lot Size','Finished Size','Sale Price','Year Built','Bathrooms','Bedrooms','Description'}
+with open('pgh_homes.json') as f:
+    homes = json.load( f ) 
+    for home in homes:
+        db[ home['HouseID'] ] = home
 
 
     
 
-outputKeys = ['HouseID','Neighborhood','Latitude','Longitude','Sale Price','Bathrooms','Bedrooms','Lot Size']
+# Serve a list of houses to the client in JSON form
+# outputKeys specifies the specific keys of data we're going to send to the client
+# The intuition here is that we don't want to waste bandwidth sending fields the client may not need at the start, like Description
+# Houses will enable filtering through URL query parameters (https://skorks.com/2010/05/what-every-developer-should-know-about-urls/)
 
+outputKeys = ['HouseID','Neighborhood','Latitude','Longitude','Sale Price','Bathrooms','Bedrooms','Lot Size']
+@app.route('/houses')
+def houses():
+    
+    # Fetch filter criteria from URL params  (None if not present or not float)
+    priceMin = request.args.get('priceMn', None, type=float)
+    priceMax = request.args.get('priceMx', None, type=float)
+    bathMin = request.args.get('bathMn', None, type=float)
+    bathMax = request.args.get('bathMx', None, type=float)
+    bedMin = request.args.get('bedMn', None, type=float)
+    bedMax = request.args.get('bedMx', None, type=float)
+    
+    # Create a new array of houses to send to client
+    filtered = []
+    for home in db.values():
+        
+        # Apply filter criteria, if the user included them in their request
+        # There are fancier, more Pythonic ways to do this, but some ifs is quick for a demo app
+        # After all, in reality this would be implemented in a database query because you won't be using
+        #  a flat dictionary object as your database in a production system! Don't do it!
+        if priceMin is not None and home['Sale Price'] < priceMin:
+            continue
+        if priceMax is not None and home['Sale Price'] > priceMax:
+            continue
+        if bathMin is not None and home['Bathrooms'] < bathMin:
+            continue
+        if bathMax is not None and home['Bathrooms'] > bathMax:
+            continue
+        if bedMin is not None and home['Bedrooms'] < bedMin:
+            continue
+        if bedMax is not None and home['Bedrooms'] > bedMax:
+            continue
+        
+        # Only send the outputKeys we specified
+        outputHome = { key: home[key] for key in outputKeys }
+        filtered.append(outputHome)
+        
+    return jsonify( filtered )
 
     
     
@@ -29,7 +97,7 @@ outputKeys = ['HouseID','Neighborhood','Latitude','Longitude','Sale Price','Bath
 # You should never expose dev servers to the public
 # Always use a bridge with a real web server if you're doing this
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port='5000')
+    app.run(host='0.0.0.0',port='9000')
     
     
     
